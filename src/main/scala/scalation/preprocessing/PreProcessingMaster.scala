2596735a8921b6d32dab833f57f8922f1ec5dac7
Vinay Kumar Bingi <vinay.bingi@gmail.com>
2018-11-25 01:58:40 -0500

    upgraded database library

--- src/main/scala/scalation/preprocessing/PreProcessingMaster.scala
+++ src/main/scala/scalation/preprocessing/PreProcessingMaster.scala
@@ -1,6 +1,7 @@
 package scalation.preprocessing
 
 import akka.actor.Actor
+import scalation.columnar_db._
 
 /**
   * Created by vinay on 11/8/18.
@@ -12,7 +13,16 @@ class PreProcessingMaster extends Actor {
         case project (r, cNames) =>
             sender() ! r.project (cNames: _*)
 
+        case mapToInt (v) =>
+            sender ! mapToInt(v)
 
+        case replaceMissingValues (r, mCol, mVal, fVal, frac) =>
+            MissingValues.replaceMissingValues (r, mCol, mVal, fVal, frac)
+            sender ! r
+
+        case replaceMissingStrings (r, mCol, mVal, fVal, frac) =>
+            MissingValues.replaceMissingStrings (r, mCol, mVal, fVal, frac)
+            sender ! r
     }
 
     override def receive: Receive = PPHandler ()
--- src/main/scala/scalation/preprocessing/package.scala
+++ src/main/scala/scalation/preprocessing/package.scala
@@ -10,15 +10,17 @@ package object preprocessing {
     case class project (r: Relation, cNames: Seq[String])
 
     // def mapToInt (s: VectoS): (VectoI, BiMap [StrNum, Int])
-    case class mapToInt (name: VectoS)
+    case class mapToInt (v: VectoS)
 
     // def replaceMissingValues [T <: Any : ClassTag] (xy: Table, missingCol: String, missingVal: T,
     //                                      funcVal: Imputation = ImputeMean, fraction: Double = 0.2)
+    // return the table xy to sender after computation
     case class replaceMissingValues [T <: Any : ClassTag] (xy: Table, missingCol: String, missingVal: T,
                                                            funcVal: Imputation = ImputeMean, fraction: Double = 0.2)
 
     // def replaceMissingStrings (xy: Table, missingCol: String, missingStr: String = "?",
     //                                      funcVal: Imputation = ImputeMean, fraction: Double = 0.2)
+    // return the table xy to sender after computation
     case class replaceMissingStrings (xy: Table, missingCol: String, missingStr: String = "?",
                                       funcVal: Imputation = ImputeMean, fraction: Double = 0.2)
 
